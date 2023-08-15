d4cdeb064bdad032e6247be00961f0e109813d03
Rafael Oliveira <81423212+RafaDev95@users.noreply.github.com>
2023-08-15 10:36:42 -0300

    Update firebase.ts

--- task-manager-server/src/config/firebase.ts
+++ task-manager-server/src/config/firebase.ts
@@ -2,7 +2,7 @@ import { cert, initializeApp } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
 
-const serviceAccount = require('./task-manage-5241f-firebase-adminsdk-zmd3t-f7f30ec02b.json')
+const serviceAccount = require('./path-to-key')
 
 initializeApp({
   credential: cert(serviceAccount),
